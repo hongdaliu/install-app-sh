eb917f6d6040b96fee3e4bdcc49561b968a8e761
hongdaliu <liuhongda11@hotmail.com>
2019-05-13 16:38:01 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,5 +1,5 @@
 firewall() {
-  ips=("192.168.1.0" "192.168.1.1")
+  ips=("198.58.106.192" "72.14.191.98" "45.33.13.132" "45.33.14.182" "45.33.0.93" "45.79.23.242" "23.239.26.70")
 
   for ip in ${ips[*]}
   do
@@ -7,4 +7,5 @@ firewall() {
     firewall-cmd --zone=public --add-rich-rule 'rule family="ipv4" source address="'${ip}'" accept' --permanent
   done
   firewall-cmd --reload
+  echo "firewall reload success"
 }
