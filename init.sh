bf2844289e9e6e0ae55d2aefd6b9f23e7d57b41f
hongdaliu <liuhongda11@hotmail.com>
2019-05-14 14:15:26 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -31,7 +31,7 @@ fi
 # update firewall white list
 wget https://raw.githubusercontent.com/hongdaliu/install-app-sh/master/servers.sh
 sleep 3s
-source servers.sh
+source ./servers.sh
 for server in ${servers[*]}
 do
   firewall-cmd --zone=public --add-rich-rule 'rule family="ipv4" source address="'${server}'" accept' --permanent
