8147fdfecf8d013c1ee807e9b65c843015e10d35
hongdaliu <liuhongda11@hotmail.com>
2019-05-14 14:17:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -31,7 +31,7 @@ then
   for server in ${servers[*]}
   do
     echo ${server}
-    ssh ${server} 'wget https://raw.githubusercontent.com/hongdaliu/install-app-sh/master/init.sh; sleep 3s, sh init.sh; sleep 5s; rm -rf servers.sh; rm -rf init.sh'
+    ssh ${server} 'wget https://raw.githubusercontent.com/hongdaliu/install-app-sh/master/init.sh; sleep 3s; sh init.sh; sleep 5s; rm -rf servers.sh; rm -rf init.sh'
   done
   echo "finished!!!"
 fi
