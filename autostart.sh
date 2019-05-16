55cd884f5aaed4e49803792c17a3711435dbfe35
hongdaliu <liuhongda11@hotmail.com>
2019-05-16 10:29:35 +0800

    Create autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# start hadoop service
+start-dfs.sh
+start-yarn.sh
+
+# start hbase service
+start-hbase.sh
+
+# start kafka service
+kafka-server-start.sh -daemon /opt/kafka_2.12-2.2.0/config/server.properties
+kafka-manager -Dhttp.port=8080 -Dconfig.file=/opt/kafka-manager-2.0.0.2/conf/application.conf
