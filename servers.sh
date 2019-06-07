6111d1e27a213297f33d08cba97d818b26a79c50
hongdaliu <liuhongda11@hotmail.com>
2019-06-08 06:22:17 +0800

    Update servers.sh

--- servers.sh
+++ servers.sh
@@ -3,7 +3,7 @@
 export zookeeperServer=("45.33.13.132" "72.14.191.98" "198.58.106.192")
 export datacenterServer=("45.56.126.161" "45.33.0.93" "45.33.14.182")
 export serviceServer=("45.79.26.254")
-export localComputer=("123.132.248.222")
+export localComputer=("123.132.248.222" "112.251.250.66")
 
 export allServers=(${zookeeperServer[*]} ${datacenterServer[*]} ${serviceServer[*]})
 export whiteListServers=(${zookeeperServer[*]} ${datacenterServer[*]} ${serviceServer[*]} ${localComputer[*]})
