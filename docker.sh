f6336c62bcb0507814bfb90169c1922d3dc81331
hongdaliu <liuhongda11@hotmail.com>
2019-08-06 21:28:28 +0800

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yes | yum update
+# Docker install
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
